76756f36c6f1efbe709bdb4b7fc98bae3167dfcd
unknown <kskhom@n103443.merann.ru>
2015-08-04 15:07:13 +0300

    first commit

---
+++ app/src/main/java/com/example/kskhom/mygalleryviewer/Gallery.java
@@ -0,0 +1,57 @@
+package com.example.kskhom.mygalleryviewer;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+public class Gallery extends FragmentActivity {
+    public static final String EXTRA_IMAGE = "extra_image";
+
+    private ImagePagerAdapter mAdapter;
+    private ViewPager mPager;
+
+    // A static dataset to back the ViewPager adapter
+    public final static Integer[] imageResIds = new Integer[] {
+            R.drawable.sample_image_1, R.drawable.sample_image_2, R.drawable.sample_image_3};
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery); // Contains just a ViewPager
+
+        mAdapter = new ImagePagerAdapter(getSupportFragmentManager(), imageResIds.length);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+    }
+
+    public void loadBitmap(int resId, ImageView imageView) {
+        imageView.setImageResource(resId);
+        BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+        task.execute(resId);
+    }
+
+    public static class ImagePagerAdapter extends FragmentStatePagerAdapter {
+        private final int mSize;
+
+        public ImagePagerAdapter(FragmentManager fm, int size) {
+            super(fm);
+            mSize = size;
+        }
+
+        @Override
+        public int getCount() {
+            return mSize;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kskhom/mygalleryviewer/PageFragment.java
@@ -0,0 +1,64 @@
+package com.example.kskhom.mygalleryviewer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by kskhom on 03.08.2015.
+ */
+public class PageFragment extends Fragment {
+    private static final String IMAGE_DATA_EXTRA = "resId";
+    private int mImageNum;
+    private ImageView mImageView;
+
+
+    static PageFragment newInstance(int imageNum) {
+        final PageFragment f = new PageFragment();
+        final Bundle args = new Bundle();
+        args.putInt(IMAGE_DATA_EXTRA, imageNum);
+        f.setArguments(args);
+        return f;
+    }
+
+    // Empty constructor, required as per Fragment docs
+    public PageFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mImageNum = this.getArguments().getInt(IMAGE_DATA_EXTRA);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // image_fragment.xml contains just an ImageView
+        final View v = inflater.inflate(R.layout.image_fragment, container, false);
+        mImageView = (ImageView) v.findViewById(R.id.imagePage);
+        if (savedInstanceState != null) {
+            mImageNum = savedInstanceState.getInt(IMAGE_DATA_EXTRA);
+        }
+        return v;
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (Gallery.class.isInstance(getActivity())) {
+            final int resId = Gallery.imageResIds[mImageNum];
+            // Call out to Gallery to load the bitmap in a background thread
+            ((Gallery) getActivity()).loadBitmap(resId, mImageView);
+        }
+    }
+
+
+
+}
