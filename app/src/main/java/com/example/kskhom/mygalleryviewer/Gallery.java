2b9cbac5df4b38d3da34448b700193768471ea58
Ksenia Kudakova <k.kudakova@gmail.com>
2015-08-06 18:03:34 +0300

    Changes for Gallery watching

--- app/src/main/java/com/example/kskhom/mygalleryviewer/Gallery.java
+++ app/src/main/java/com/example/kskhom/mygalleryviewer/Gallery.java
@@ -23,10 +23,8 @@ import java.util.ArrayList;
 
 
 public class Gallery extends FragmentActivity {
-    static final String IMAGE_COLUMN_URI = "uri";
     private ImagePagerAdapter mAdapter;
     private ViewPager mPager;
-    private PageFragment fr;
 
     final Uri IMAGE_URI = Uri
             .parse("content://media/external/images/media/");
@@ -40,45 +38,30 @@ public class Gallery extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery); // Contains just a ViewPager
 
-        if(mImageUrls.isEmpty()){
-        String pictureCols[] = {MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA};
-        Cursor cursor = getBaseContext().getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, pictureCols,
-                null, null, null);
-        cursor.moveToFirst();
+        if (mImageUrls.isEmpty()) {
+            String pictureCols[] = {MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA};
+            Cursor cursor = getBaseContext().getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, pictureCols,
+                    null, null, null);
+            cursor.moveToFirst();
 
-        try {
-            // Iterate the cursor for Image urls
-            for (int index = 0; index < cursor.getCount(); index++) {
+            try {
+                // Iterate the cursor for Image urls
+                for (int index = 0; index < cursor.getCount(); index++) {
 
-                cursor.moveToPosition(index);
-                int dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
-                mImageUrls.add(cursor.getString(dataColumnIndex));
+                    cursor.moveToPosition(index);
+                    int dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                    mImageUrls.add(cursor.getString(dataColumnIndex));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                cursor.close();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            cursor.close();
-        }}
+        }
         mAdapter = new ImagePagerAdapter(getSupportFragmentManager(), mImageUrls.size());
         mPager = (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(mAdapter);
-
-//        fr = (PageFragment) mAdapter.getItem(0);
-//
-//        // create the fragment and data the first time
-//        if (fr != null) {
-//            mImageUrls.addAll(fr.getMImageUrls());
-//        }
     }
-
-//    public void onDestroy() {
-//        super.onDestroy();
-//        if (mAdapter.getCount() > 0) {
-//            fr = (PageFragment) mAdapter.getItem(0);
-//            fr.setMImageUrls(mImageUrls);
-//        }
-//    }
-
 
     public static class ImagePagerAdapter extends FragmentPagerAdapter {
         private final int mSize;
--- app/src/main/java/com/example/kskhom/mygalleryviewer/PageFragment.java
+++ app/src/main/java/com/example/kskhom/mygalleryviewer/PageFragment.java
@@ -1,18 +1,13 @@
 package com.example.kskhom.mygalleryviewer;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -20,11 +15,8 @@ import java.util.ArrayList;
  */
 public class PageFragment extends Fragment {
     private static final String IMAGE_DATA_EXTRA = "resId";
-    static final int GALLERY_REQUEST = 1;
     private String mImageNum;
     private ImageView mImageView;
-    private final static ArrayList<String> mImageUrls = new ArrayList<String>();
-    private static int i=0;
 
 
     static PageFragment newInstance(String imageNum) {
@@ -55,9 +47,7 @@ public class PageFragment extends Fragment {
         mImageView = (ImageView) v.findViewById(R.id.imagePage);
         if (savedInstanceState != null) {
             mImageNum = savedInstanceState.getString(IMAGE_DATA_EXTRA);
-            if (mImageNum==null && i<((Gallery)getActivity()).mImageUrls.size()) {mImageNum = ((Gallery)getActivity()).mImageUrls.get(i); i++;}
         }
-        if (i==((Gallery)getActivity()).mImageUrls.size()) i=0;
         return v;
     }
 
@@ -65,9 +55,6 @@ public class PageFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-//        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-//        photoPickerIntent.setType("image/*");
-//        getActivity().startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
     }
 
 
@@ -82,23 +69,14 @@ public class PageFragment extends Fragment {
     public void onStop()
     {
         super.onStop();
+
         mImageView.setImageURI(null);
     }
-    public void onDestroy()
-    {
-        super.onDestroy();
-        final Bundle args = new Bundle();
-        args.putString(IMAGE_DATA_EXTRA, mImageNum);
-        this.setArguments(args);
-    }
 
-    public ArrayList<String> getMImageUrls()
+    @Override
+    public void onSaveInstanceState(Bundle instState)
     {
-        return mImageUrls;
-    }
-
-    public void setMImageUrls(ArrayList<String> urls)
-    {
-        mImageUrls.addAll(urls);
+        instState.putString(IMAGE_DATA_EXTRA, mImageNum);
+        super.onSaveInstanceState(instState);
     }
 }
